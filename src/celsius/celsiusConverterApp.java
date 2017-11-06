c8bfb1e87bf85afb1e3764e572ca6ce3a95ffd87
agmojzer <agmojzer@gmail.com>
2017-11-06 11:53:22 -0500

    initial commit

---
+++ src/celsius/celsiusConverterApp.java
@@ -0,0 +1,33 @@
+package celsius;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class celsiusConverterApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Temperature Converter Application!");
+		Scanner sc = new Scanner(System.in);
+	
+	String choice = "y";
+	while (choice.equalsIgnoreCase("y")) {
+		System.out.print("Enter degrees in Celsius: ");
+		double degC = sc.nextDouble();
+		double degF= degC*1.8 + 32;
+		NumberFormat number = NumberFormat.getNumberInstance();
+		number.setMaximumFractionDigits(2);
+		String roundedDegF = number.format(degF);
+		
+		System.out.println("Degrees in Fahrenheit= " + roundedDegF);
+		
+		System.out.print("Continue? (y/n):");
+        choice = sc.next();
+        System.out.println();
+		
+	}
+	sc.close();
+	System.out.println("Bye!");
+
+	}
+
+}
